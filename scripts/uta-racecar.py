6ae210d29b081483f1217379837478edfa186834
Harshilkumar Patel <harshil912@gmail.com>
2020-04-01 16:00:18 -0500

    integrate path_sense and api change for continous drive

--- scripts/uta-racecar.py
+++ scripts/uta-racecar.py
@@ -9,6 +9,7 @@ from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 import actionlib    
 from utils.capture_vision_steering import Capture
 from drive import Drive
+
 # print "............"
 def joy_callback(msg):
     # TODO: subscribed to /joy
@@ -17,7 +18,6 @@ def joy_callback(msg):
 # change_time = rospy.Time.now()
 
 def get_time_passed(started = None):
-
     if not started:
         return 0
     
@@ -27,19 +27,22 @@ def package_init(message = ""):
     print message
 
 
-
 if __name__ == "__main__":
     package_init("uta-racecar started")
     rospy.init_node("uta_racecar")
 
     capture = Capture()
-    capture.listener()
+    # capture.initiate_setup()
+    # capture.activate_listener_for_saving_data()
+    capture.activate_listener_for_autonomy()
 
     # drive = Drive()
     # drive.test_steering()
 
     # drive = Drive() 
+    # drive.initiate_threads()
     # drive.go_left()
+    # drive.go_right()
     
     while not rospy.is_shutdown():
         rospy.spin()
