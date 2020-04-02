85a2e6d55112d1861927c1657fad34cfda5a44a1
Harshilkumar Patel <harshil912@gmail.com>
2020-04-02 16:50:02 -0500

    cleanup

--- scripts/drive.py
+++ scripts/drive.py
@@ -103,28 +103,22 @@ class Drive(object):
         # TODO: add a way to keep moving forward for x seconds or x meters
     
     def go_back(self, secs = 0.0, dist = 0.0):
-        start = datetime.datetime.now()
-        end = datetime.datetime.now()
-
-        while (datetime.datetime.now() - start).seconds < secs:
-            self.current_speed = -2.0
-            # config = self.get_config(-2.0)
-
-            # self.ack_publisher.publish(config)
+        if secs:
+            start = datetime.datetime.now()
             end = datetime.datetime.now()
-        # TODO: add a way to keep moving forward for x seconds or x meters
 
-        self.current_speed = 0.0
+            while (datetime.datetime.now() - start).seconds < secs:
+                self.current_speed = -2.0
+                end = datetime.datetime.now()
+            # TODO: add a way to keep moving forward for x seconds or x meters
+
+            self.current_speed = 0.0
 
     def make_turn(self, angle = 0.0):
         # TODO: add a way to turn at an angle and straighten up after that
 
         self.current_steering_angle = angle
-        return 
-
-        # print "making a turn at an angle ", angle
-        # config = self.get_config(None, angle)
-        # self.ack_publisher.publish(config)  
+        return True
 
     def go_left(self, angle = 0.0):
         # TODO: add a way to make a 90 turn and straighten up
@@ -133,8 +127,7 @@ class Drive(object):
         while (datetime.datetime.now() - start).seconds < 3.4:
             self.current_speed = 2.0
             self.current_steering_angle = self.max_angle
-            # config = self.get_config(1.0, self.max_angle)
-            # self.ack_publisher.publish(config)
+            
         self.current_speed = 0.0
 
         return
@@ -146,8 +139,7 @@ class Drive(object):
         while (datetime.datetime.now() - start).seconds < 3.4:
             self.current_speed = 2.0
             self.current_steering_angle = - self.max_angle
-            # config = self.get_config(1.0, -1*self.max_angle)
-            # self.ack_publisher.publish(config)
+            
         self.current_speed = 0.0
 
         return
