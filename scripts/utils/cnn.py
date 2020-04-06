92e10e3b1abd57f8e82e74bf9956fe01bd392aec
Harshilkumar Patel <harshil912@gmail.com>
2020-04-06 09:30:45 -0500

    pull merge

--- scripts/utils/cnn.py
+++ scripts/utils/cnn.py
@@ -3,17 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class CNN(object):
-    def __init__(self);
+    def __init__(self):
         self.model = tf.keras.models.Sequential()
+        return
     
     def add_layer(self):
         # TODO: add layer to self.model
+        return
     
     def remove_layer(self, layer_number = -1):
         # TODO: remove layer at ith position
+        return
     
     def set_model(self, model = None):
         # TODO: add model
-    
-    
-    
+        return
