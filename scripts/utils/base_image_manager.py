9201bf92af4292fbc453e577e4d0b0f7e6951aaa
Harshilkumar Patel <harshil912@gmail.com>
2020-11-02 13:54:27 -0600

    readign synced messages

--- scripts/utils/base_image_manager.py
+++ scripts/utils/base_image_manager.py
@@ -50,31 +50,31 @@ class BaseImageManager(object):
         BaseImageManager.RIGHT_CAMERA_DIR = os.path.join(BaseImageManager.FILE_PATH, "right_camera")
         BaseImageManager.AUTONOMOUS_DIR = os.path.join(BaseImageManager.FILE_PATH, "autonomous_camera")
         BaseImageManager.USB_CAMERA_DIR = os.path.join(BaseImageManager.FILE_PATH, "usb_camera")
+        BaseImageManager.PC_DIR = os.path.join(BaseImageManager.FILE_PATH, "pc")
+        BaseImageManager.SCAN_DIR = os.path.join(BaseImageManager.FILE_PATH, "scan")
 
         os.mkdir(BaseImageManager.LEFT_CAMERA_DIR)
         os.mkdir(BaseImageManager.RIGHT_CAMERA_DIR)
         os.mkdir(BaseImageManager.AUTONOMOUS_DIR)
         os.mkdir(BaseImageManager.USB_CAMERA_DIR)
+        os.mkdir(BaseImageManager.PC_DIR)
+        os.mkdir(BaseImageManager.SCAN_DIR)
     
     def create_log_files_for_images(self):
         left_camera_txt = os.path.join(BaseImageManager.FILE_PATH, "left_camera.txt")
         right_camera_txt = os.path.join(BaseImageManager.FILE_PATH, "right_camera.txt")
         usb_camera_txt = os.path.join(BaseImageManager.FILE_PATH, "usb_camera.txt")
         drive_autonomous_txt = os.path.join(BaseImageManager.FILE_PATH, "drive_autonomous.txt")
-        pc_text = os.path.join(BaseImageManager.FILE_PATH, "pc.txt")
 
         open(left_camera_txt, "w").close()
         open(right_camera_txt, "w").close()
         open(usb_camera_txt, "w").close()
         open(drive_autonomous_txt, "w").close()
 
-        BaseImageManager.LEFT_CAMERA_TXT = open(left_camera_txt, 'a')
-        BaseImageManager.RIGHT_CAMERA_TXT = open(right_camera_txt, 'a')
-        BaseImageManager.USB_CAMERA_TXT = open(usb_camera_txt, 'a')
-        BaseImageManager.AUTONOMOUS_TXT = open(drive_autonomous_txt, 'a')
-        BaseImageManager.PCT_TEXT = open(pc_text, 'a')
-
-        BaseImageManager.PC_WRITER = csv.writer(BaseImageManager.PCT_TEXT, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        BaseImageManager.LEFT_CAMERA_TXT = open(left_camera_txt, 'w')
+        BaseImageManager.RIGHT_CAMERA_TXT = open(right_camera_txt, 'w')
+        BaseImageManager.USB_CAMERA_TXT = open(usb_camera_txt, 'w')
+        BaseImageManager.AUTONOMOUS_TXT = open(drive_autonomous_txt, 'w')
     
     def initiate_setup_to_record_vision(self):
         if BaseImageManager.FILE_PATH:
@@ -92,8 +92,6 @@ class BaseImageManager(object):
         self.create_log_files_for_images()
 
         fourcc = cv.VideoWriter_fourcc(*'mp4v')
-        BaseImageManager.LEFT_CAMERA = cv.VideoWriter(BaseImageManager.FILE_PATH + '/left_camera.mp4', fourcc, 30.0, (IMAGE_SHAPE))
-        BaseImageManager.RIGHT_CAMERA = cv.VideoWriter(BaseImageManager.FILE_PATH + '/right_camera.mp4', fourcc, 30.0, (IMAGE_SHAPE))
     
     def save_file(self, image, file_path_with_name):
         # image type: PIL.Image
@@ -111,9 +109,13 @@ class BaseImageManager(object):
     def read_image(self, data, rgb_format = 'RGB', shape = IMAGE_SHAPE):
         # NOTE: streamed data from ZED is in BGR format
         # NOTE: returns a PIL image
-        image = Im.frombytes("RGB", shape, data)
+        image = Im.frombytes(rgb_format, shape, data)
         if rgb_format == 'RGB':
             (r,g,b) = image.split()
             return Im.merge("RGB", (b,g,r))
+        elif rgb_format == 'RGBA':
+            (r,g,b,a) = image.split()
+            print a
+            return Im.merge("RGBA", (b,g,r,a))
         else:
             return image
